777e969057733662cc55d3eddfbe2aef1c00b98d
Tommy Deng <git@tommydeng.com>
2020-08-17 22:52:42 -0400

    feat: add/update splash page elements
    - top bar
    - logos/branding
    - socials
    - background graphic
    - typography (Metropolis font, bold)
    - favicon
    - breakpoint responsiveness
    
    refs: #5

--- components/sections/SplashScreen.tsx
+++ components/sections/SplashScreen.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { Flex, Link, Text } from "@chakra-ui/core";
+import { Flex, Image, Link, Text } from "@chakra-ui/core";
 import styled from "@emotion/styled";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faFacebookSquare,
-  faInstagram,
-  faTwitterSquare,
-} from "@fortawesome/free-brands-svg-icons";
+import Socials from "@/components/Socials";
+
+const FlexFullView = styled(Flex)`
+  background-image: url("cityscape.svg");
+  background-repeat: no-repeat;
+  background-attachment: local;
+`;
 
 const TextStyled = styled(Text)`
   font-family: "Inter", sans-serif;
-  margin-bottom: 1vh;
+  margin-bottom: 8px;
 `;
 
 const TextStyledExtraBold = styled(TextStyled)`
@@ -18,52 +19,96 @@ const TextStyledExtraBold = styled(TextStyled)`
 `;
 
 const TextStyledBold = styled(TextStyled)`
-  font-weight: 800;
+  font-weight: 700;
+  font-family: "Metropolis", sans-serif;
 `;
 
 const LinkStyled = styled(Link)`
   font-family: "Inter", sans-serif;
-  margin-bottom: 1vh;
   font-weight: 600;
-`;
-
-const FlexMiddleRow = styled(Flex)`
-  margin-left: 15vw;
-  margin-right: 15vw;
-`;
-
-const IconStyled = styled(FontAwesomeIcon)`
-  margin-left: 1vw;
-  margin-right: 1vw;
 `;
 
 export default function SplashScreen(): React.ReactElement {
   return (
-    <Flex height="100vh" width="100vw">
-      <Flex flexDirection="column" justifyContent="center">
-        <FlexMiddleRow justifyContent="space-between" flexDirection="row">
-          <Flex flexDirection="column">
-            <TextStyledExtraBold fontSize="5xl">CUSEC 2021</TextStyledExtraBold>
-            <TextStyledBold>
-              Canadian University Software Engineering Conference
-            </TextStyledBold>
-            <TextStyled>
-              January 15 - 17, 2021 {"\u2022"} Virtual Experience
-            </TextStyled>
-            <LinkStyled color="teal.500" href="http://localhost:3000">
-              Interested in Sponsoring?
-            </LinkStyled>
-            <Flex flexDirection="row" justifyContent="flex-start">
-              <IconStyled icon={faFacebookSquare} />
-              <IconStyled icon={faTwitterSquare} />
-              <IconStyled icon={faInstagram} />
+    <>
+      <FlexFullView
+        height={[
+          "max(100vh, 600px)",
+          "max(100vh, 600px)",
+          "max(100vh, 700px)",
+          "max(100vh, 800px)",
+        ]}
+        width="100%"
+        backgroundSize="100vw auto"
+        backgroundPosition={["center bottom"]}
+      >
+        <Flex
+          flexDirection="column"
+          justifyContent="center"
+          width="100%"
+          height="100%"
+          paddingX={["16px", "16px", "10vw", "10vw"]}
+        >
+          <Flex justifyContent="center">
+            <Image
+              src="logo.svg"
+              alt="CUSEC logo"
+              size="100px"
+              marginBottom="8px"
+              display={["block", "block", "block", "none"]}
+            />
+          </Flex>
+          <Flex
+            justifyContent={["center", "center", "center", "space-between"]}
+            alignItems="center"
+            width="100%"
+            paddingBottom={["1in", "1in", "2in", "2in"]}
+          >
+            <Flex
+              flexDirection="column"
+              textAlign={["center", "center", "center", "left"]}
+            >
+              <TextStyledExtraBold fontSize={["4xl", "5xl"]}>
+                CUSEC 2021
+              </TextStyledExtraBold>
+              <TextStyledBold fontSize={["xl", "2xl"]}>
+                Canadian University Software Engineering Conference
+              </TextStyledBold>
+              <Flex justify={["center", "center", "center", "left"]}>
+                <TextStyled>January 9 - 10, 2021</TextStyled>
+                <Text margin="0 10px">{"\u2022"}</Text>
+                <TextStyled>Virtual Experience</TextStyled>
+              </Flex>
+              <Flex
+                alignItems="center"
+                marginTop="8px"
+                justify={["center", "center", "center", "left"]}
+              >
+                <LinkStyled
+                  color="brand.dark_teal"
+                  href="mailto:sponsor@cusec.net"
+                >
+                  Interested in sponsoring?
+                </LinkStyled>
+              </Flex>
+              <Flex
+                justifyContent="center"
+                alignItems="center"
+                display={["flex", "flex", "flex", "none"]}
+              >
+                <Socials margin="16px" />
+              </Flex>
             </Flex>
+            <Image
+              src="logo.svg"
+              alt="CUSEC logo"
+              width="min(250px, 15vw)"
+              marginLeft="50px"
+              display={["none", "none", "none", "block"]}
+            />
           </Flex>
-          <Flex>
-            <TextStyled>Logo</TextStyled>
-          </Flex>
-        </FlexMiddleRow>
-      </Flex>
-    </Flex>
+        </Flex>
+      </FlexFullView>
+    </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import TopBar from "@/components/TopBar";
 import SplashScreen from "@/components/sections/SplashScreen";
 
 export default function Home() {
@@ -7,13 +8,36 @@ export default function Home() {
       <Head>
         <title>CUSEC 2021</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
+        <meta
+          name="description"
+          content="Mark your calendars, Canada's favourite conference is back virtually this winter! Join us January 9-10, 2021 for a weekend of amazing speakers, workshops, and more!"
+        ></meta>
       </Head>
 
       <main>
+        <TopBar />
         <SplashScreen />
       </main>
 
       <style jsx global>{`
+        @font-face {
+          font-family: "Metropolis";
+          font-weight: bold;
+          font-style: normal;
+          src: url("fonts/Metropolis-Bold.woff2") format("woff2"),
+            url("fonts/Metropolis-Bold.woff") format("woff"),
+            url("fonts/Metropolis-Bold.otf") format("otf"),
+            url("fonts/Metropolis-Bold.ttf") format("ttf");
+        }
+
+        :root {
+        }
+
         html,
         body {
           padding: 0;
