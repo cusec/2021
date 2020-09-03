00ba7be123f6d782b43287c3e99aa2e25b7a5130
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-09-03 10:00:08 -0400

    feat: add hover animation to links in footer
    refs:
    issue #18

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import Logo from "../svgs/logo.svg";
-import { Flex, Text } from "@chakra-ui/core";
+import { Flex, Link, Text } from "@chakra-ui/core";
 import React from "react";
 
 const BaseContainer = styled.div`
@@ -12,9 +12,37 @@ const BaseContainer = styled.div`
 `;
 
 const FooterText = styled(Text)`
+  color: #616161;
   font-family: "Inter", sans-serif;
   margin: 5px 8px;
+  padding-bottom: 1px;
+  position: relative;
+`;
+
+const FooterLink = styled(Link)`
   color: #616161;
+  font-family: "Inter", sans-serif;
+  margin: 5px 8px;
+  padding-bottom: 1px;
+  position: relative;
+
+  &:after {
+    content: "";
+    position: absolute;
+    height: 2px;
+    width: 100%;
+    left: 0;
+    bottom: 0;
+    opacity: 0;
+    transform: translateY(3px);
+    background: #11a0a7;
+    transition: opacity 0.2s ease, transform 0.2s ease;
+  }
+
+  &:hover::after {
+    opacity: 1;
+    transform: translateY(0);
+  }
 `;
 
 export default function Footer(): React.ReactElement {
@@ -36,15 +64,34 @@ export default function Footer(): React.ReactElement {
             justifyContent="center"
             marginLeft={["0", "0", "2vw", "5vw"]}
           >
-            <FooterText fontSize={["xs", "xs", "xs", "sm"]}>
+            <FooterLink
+              _focus={{}}
+              _hover={{}}
+              fontSize={["xs", "xs", "xs", "sm"]}
+              href="mailto:info@cusec.net"
+            >
               Contact CUSEC
-            </FooterText>
-            <FooterText fontSize={["xs", "xs", "xs", "sm"]}>
+            </FooterLink>
+            <FooterLink
+              _focus={{}}
+              _hover={{}}
+              fontSize={["xs", "xs", "xs", "sm"]}
+              href="https://github.com/cusec/2021"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
               Open Source
-            </FooterText>
-            <FooterText fontSize={["xs", "xs", "xs", "sm"]}>
-              Icons from flaticons.com*
-            </FooterText>
+            </FooterLink>
+            <FooterLink
+              _focus={{}}
+              _hover={{}}
+              fontSize={["xs", "xs", "xs", "sm"]}
+              href="https://www.flaticon.com/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Icons from flaticon.com*
+            </FooterLink>
           </Flex>
           <Flex alignItems="center" justifyContent="center">
             <Logo style={{ height: "24px", width: "auto" }} />
@@ -57,12 +104,24 @@ export default function Footer(): React.ReactElement {
             <FooterText fontSize={["xs", "xs", "xs", "sm"]}>
               &copy; CUSEC 2021
             </FooterText>
-            <FooterText fontSize={["xs", "xs", "xs", "sm"]}>
+            <FooterLink
+              _focus={{}}
+              _hover={{}}
+              fontSize={["xs", "xs", "xs", "sm"]}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
               Code of Conduct
-            </FooterText>
-            <FooterText fontSize={["xs", "xs", "xs", "sm"]}>
+            </FooterLink>
+            <FooterLink
+              _focus={{}}
+              _hover={{}}
+              fontSize={["xs", "xs", "xs", "sm"]}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
               Privacy Policy
-            </FooterText>
+            </FooterLink>
           </Flex>
         </Flex>
       </BaseContainer>
