aeba98a47554e06901f52090d501c9f5c1e8156b
Tommy Deng <git@tommydeng.com>
2020-09-07 18:01:17 -0400

    feat: update accordion style and logic
    - add white background
    - adjust spacing to take up full width
    - move space around the mailto link instead of within it
    - remove busy hover animation
    - refactor to simplify logic and to open accordion item based on matching index instead of question content
    
    refs: #17

--- components/sections/FAQ.tsx
+++ components/sections/FAQ.tsx
@@ -27,58 +27,11 @@ const Background = styled.div`
   background-color: #fafafa;
 `;
 
-const AccordionHeaderStyled = styled(AccordionHeader)`
-   {
-    vertical-align: middle;
-    -webkit-transform: perspective(1px) translateZ(0);
-    transform: perspective(1px) translateZ(0);
-    box-shadow: 0 0 1px rgba(0, 0, 0, 0);
-    position: relative;
-    -webkit-transition-property: color;
-    transition-property: color;
-    -webkit-transition-duration: 0.5s;
-    transition-duration: 0.5s;
-  }
-  &:before {
-    content: "";
-    position: absolute;
-    z-index: -1;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: #f1f1f1;
-    border-radius: 8px;
-    -webkit-transform: scaleY(0);
-    transform: scaleY(0);
-    -webkit-transform-origin: 50% 100%;
-    transform-origin: 50% 100%;
-    -webkit-transition-property: transform;
-    transition-property: transform;
-    -webkit-transition-duration: 0.5s;
-    transition-duration: 0.5s;
-    -webkit-transition-timing-function: ease-out;
-    transition-timing-function: ease-out;
-  }
-  &:hover:before,
-  &:focus:before,
-  &:active:before {
-    -webkit-transform: scaleY(1);
-    transform: scaleY(1);
-    -webkit-transition-timing-function: cubic-bezier(0.52, 1.64, 0.37, 0.66);
-    transition-timing-function: cubic-bezier(0.52, 1.64, 0.37, 0.66);
-  }
-`;
-
 interface AccordionBoxParams {
   props: { question: string; answer: string };
   index: number;
-  opened: string | null;
-  setOpened: Dispatch<SetStateAction<string | null>>;
-}
-
-interface IExpanded {
-  isExpanded: boolean;
+  opened: number | null;
+  setOpened: Dispatch<SetStateAction<number | null>>;
 }
 
 function AccordionBox({
@@ -87,62 +40,50 @@ function AccordionBox({
   opened,
   setOpened,
 }: AccordionBoxParams): ReactElement {
-  const [expanded, setExpanded] = useState<boolean>(false);
-
-  const shouldBeOpened = () => {
-    if (opened === null) {
-      return false;
-    } else return opened === props.question;
-  };
+  const shouldBeOpened = opened !== null && opened === index;
 
   const handleChange = (event: boolean | FormEvent<any>) => {
-    if (event) {
-      setOpened(props.question);
-    } else {
-      setOpened(null);
-    }
+    setOpened(event ? index : null);
   };
 
   return (
     <Box
+      background="white"
       borderWidth="1px"
       display="flex"
-      margin={index === 0 || index === 1 ? "0 1vw 1vw 1vw" : "1vw"}
+      marginBottom="1vw"
       maxWidth="95vw"
       rounded="8px"
-      width="30rem"
-      borderColor={expanded ? "#09d8c4" : "#e2e8f0"}
-      shadow={expanded ? "md" : "none"}
+      borderColor={shouldBeOpened ? "brand.teal" : "#e2e8f0"}
+      shadow={shouldBeOpened ? "md" : "none"}
     >
       <AccordionItem
         border="0"
         width="100%"
         onChange={handleChange}
-        isOpen={shouldBeOpened()}
+        isOpen={shouldBeOpened}
       >
-        {({ isExpanded }: IExpanded) => (
-          <>
-            {setExpanded(isExpanded)}
-            <AccordionHeaderStyled _focus={{}} _hover={{}}>
-              <Box
-                flex="1"
-                textAlign="left"
-                fontWeight={isExpanded ? "bold" : "normal"}
-              >
-                {props.question}
-              </Box>
-              <AccordionIcon />
-            </AccordionHeaderStyled>
-            <AccordionPanel paddingBottom={4}>{props.answer}</AccordionPanel>
-          </>
-        )}
+        <AccordionHeader _focus={{}} padding="12px 20px">
+          <Box
+            flex="1"
+            textAlign="left"
+            marginRight="20px"
+            fontWeight={shouldBeOpened ? "bold" : "normal"}
+          >
+            {props.question}
+          </Box>
+          <AccordionIcon />
+        </AccordionHeader>
+        <AccordionPanel padding="8px 20px 20px 20px">
+          {props.answer}
+        </AccordionPanel>
       </AccordionItem>
     </Box>
   );
 }
 
 export default function FAQ(): ReactElement {
-  const [opened, setOpened] = useState<string | null>(null);
+  const [opened, setOpened] = useState<number | null>(null);
 
   return (
     <>
@@ -157,51 +98,51 @@ export default function FAQ(): ReactElement {
             Frequently Asked Questions
           </TextStyledBold>
           <TextStyled>
-            Can’t find an answer? Send us your question at
+            Can't find an answer? Send us your question at{" "}
             <LinkStyled color="brand.blue" href="mailto:info@cusec.net">
-              {` info@cusec.com `}
-            </LinkStyled>
+              info@cusec.com
+            </LinkStyled>{" "}
             and we’ll get back to you.
           </TextStyled>
-        </Flex>
-        <Flex
-          direction="row"
-          justify="center"
-          paddingBottom={["0.25in", "0.25in", "0.75in", "0.75in"]}
-          wrap="wrap"
-        >
-          <Flex direction={["column", "column", "column", "row"]}>
-            <Accordion>
-              {data.map((item, index) => {
-                if (index % 2 === 0) {
-                  return (
+          <Box
+            marginTop="0.25in"
+            paddingBottom={["0.25in", "0.25in", "0.75in", "0.75in"]}
+          >
+            <Flex direction={["column", "column", "column", "row"]}>
+              <Accordion
+                width={["100%", "100%", "100%", "50%"]}
+                marginRight={["0", "0", "0", "1vw"]}
+              >
+                {data
+                  .map((faqItem, index) => (
                     <AccordionBox
-                      props={item}
-                      key={item.question}
+                      props={faqItem}
+                      key={`${index}${faqItem.question}`}
                       index={index}
                       opened={opened}
                       setOpened={setOpened}
                     />
-                  );
-                }
-              })}
-            </Accordion>
-            <Accordion>
-              {data.map((item, index) => {
-                if (index % 2 !== 0) {
-                  return (
+                  ))
+                  .filter(({}, index) => index % 2 === 0)}
+              </Accordion>
+              <Accordion
+                width={["100%", "100%", "100%", "50%"]}
+                marginLeft={["0", "0", "0", "1vw"]}
+              >
+                {data
+                  .map((faqItem, index) => (
                     <AccordionBox
-                      props={item}
-                      key={item.question}
+                      props={faqItem}
+                      key={`${index}${faqItem.question}`}
                       index={index}
                       opened={opened}
                       setOpened={setOpened}
                     />
-                  );
-                }
-              })}
-            </Accordion>
-          </Flex>
+                  ))
+                  .filter(({}, index) => index % 2 !== 0)}
+              </Accordion>
+            </Flex>
+          </Box>
         </Flex>
       </Background>
     </>
