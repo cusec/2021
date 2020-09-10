99939eba3daa01c3d0e7581452d3c88f58a019b7
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-09-10 14:20:20 -0400

    feat: add spacing and positioning
    refs:
    issue #19

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from "@chakra-ui/core";
+import { Flex } from "@chakra-ui/core";
 import React, { ReactElement } from "react";
 import EmailListingInput from "@/components/EmailListingInput";
 import IGIcon from "../svgs/instagram-coloured-icon.svg";
@@ -11,27 +11,37 @@ import { TextStyled, TextStyledBold } from "@/components/StyledCore";
 export default function Contact(): ReactElement {
   return (
     <>
-      <Flex direction="row">
+      <Flex direction="row" align="center">
         <Flex
           direction="column"
-          paddingBottom={["0.2in", "0.2in", "0.3in", "0.3in"]}
-          paddingTop={["0.25in", "0.25in", "0.75in", "0.75in"]}
+          paddingBottom="0.3in"
+          paddingTop={["0.5in", "0.5in", "0.75in", "0.75in"]}
           paddingX={["5vw", "5vw", "10vw", "10vw"]}
         >
           <TextStyledBold fontSize={["xl", "3xl"]}>
             Can&apos;t get enough of CUSEC?
           </TextStyledBold>
-          <TextStyled>
+          <TextStyled paddingBottom={["0.15in", "0.15in", "0.3in", "0.3in"]}>
             Follow us on our socials. Get updates by signing up for our email
             listing.
           </TextStyled>
-          <Flex direction="row">
-            <FBIcon />
-            <TwitterIcon />
-            <IGIcon />
-            <EmailIcon />
+          <Flex direction="row" align="center">
+            <Flex paddingRight={["0.15in", "0.15in", "0.2in", "0.25in"]}>
+              <FBIcon />
+            </Flex>
+            <Flex paddingX={["0.15in", "0.15in", "0.2in", "0.25in"]}>
+              <TwitterIcon />
+            </Flex>
+            <Flex paddingX={["0.15in", "0.15in", "0.2in", "0.25in"]}>
+              <IGIcon />
+            </Flex>
+            <Flex paddingLeft={["0.15in", "0.15in", "0.2in", "0.25in"]}>
+              <EmailIcon />
+            </Flex>
           </Flex>
-          <EmailListingInput />
+          <Flex paddingTop={["0.3in", "0.3in", "0.35in", "0.35in"]}>
+            <EmailListingInput />
+          </Flex>
         </Flex>
         <Flex paddingY="0.5in" display={["none", "none", "none", "flex"]}>
           <SocialShareSVG />
